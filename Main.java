57733286d5a5b8ba38101b9d7de18618976f5bce
Rustam Zeinalov <truezeynalov@gmail.com>
2016-07-11 12:48:13 +0300

    Update Main.java
    added range start, end paramers

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
 	public static void main(String[] args) {
 	
-	fizzbuz(100);
+	fizzbuz(1, 100);
 	}
   
-	public static void fizzbuz (int n)
+	public static void fizzbuz (int start, int end)
     {
 		int []fizzes = new int[] {1, 0, 0};
 		int []buzzes = new int[] {2, 0, 0, 0, 0};
 		String [] words =  new String[] {null, "Fizz", "Buzz", "FizzBuzz"};
     
-		for (int i=1; i<=n; i++)
+		for (int i=start; i<=end; i++)
 		{
 			words[0]= Integer.toString(i);
 			System.out.println(words[fizzes[i%3] + buzzes[i%5]]);
