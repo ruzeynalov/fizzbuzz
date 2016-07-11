7432d553a5e23793e51c54e3902548bc68c3129f
Rustam Zeinalov <truezeynalov@gmail.com>
2016-07-11 12:50:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 public class Main {
-	public static void main(String[] args) {
-	
+   public static void main(String[] args) {
 	fizzbuz(1, 100);
-	}
+   }
   
-	public static void fizzbuz (int start, int end)
-    {
-		int []fizzes = new int[] {1, 0, 0};
-		int []buzzes = new int[] {2, 0, 0, 0, 0};
-		String [] words =  new String[] {null, "Fizz", "Buzz", "FizzBuzz"};
+   public static void fizzbuz (int start, int end) {
+	int []fizzes = new int[] {1, 0, 0};
+	int []buzzes = new int[] {2, 0, 0, 0, 0};
+	String [] words =  new String[] {null, "Fizz", "Buzz", "FizzBuzz"};
     
-		for (int i=start; i<=end; i++)
-		{
-			words[0]= Integer.toString(i);
-			System.out.println(words[fizzes[i%3] + buzzes[i%5]]);
-		}
+	for (int i=start; i<=end; i++)
+	{
+		words[0]= Integer.toString(i);
+		System.out.println(words[fizzes[i%3] + buzzes[i%5]]);
 	}
+     }
 }
