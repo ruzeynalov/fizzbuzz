3106c1762b38c84fda5942f29048a5721acd52ec
Rustam Zeinalov <truezeynalov@gmail.com>
2016-07-11 12:43:58 +0300

    Update Main.java
    Indents

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
-  public static void main(String[] args) {
-
-    fizzbuz(100);
-  }
+	public static void main(String[] args) {
+	
+	fizzbuz(100);
+	}
   
-  public static void fizzbuz (int n)
-  {
-  	int []fizzes = new int[] {1, 0, 0};
-    int []buzzes = new int[] {2, 0, 0, 0, 0};
-    String [] words =  new String[] {null, "Fizz", "Buzz", "FizzBuzz"};
-    
-    for (int i=1; i<=n; i++)
+	public static void fizzbuz (int n)
     {
-		words[0]= Integer.toString(i);
-		System.out.println(words[fizzes[i%3] + buzzes[i%5]]);
-    }
-  }
+		int []fizzes = new int[] {1, 0, 0};
+		int []buzzes = new int[] {2, 0, 0, 0, 0};
+		String [] words =  new String[] {null, "Fizz", "Buzz", "FizzBuzz"};
+    
+		for (int i=1; i<=n; i++)
+		{
+			words[0]= Integer.toString(i);
+			System.out.println(words[fizzes[i%3] + buzzes[i%5]]);
+		}
+	}
 }
